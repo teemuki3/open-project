b82863697905caa868befbdaa01fb75482a68aee
teemu <teantm@utu.fi>
2024-05-17 17:48:03 +0300

    fix setup

--- open_project/setup.py
+++ open_project/setup.py
@@ -13,6 +13,7 @@ setup(
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
         (os.path.join('share', package_name, 'launch'), glob('launch/*.py')),
+        (os.path.join('share', package_name, 'worlds'), glob('worlds/*.world')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
