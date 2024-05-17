eec892935f54fdc400383ef1871020c72af24b75
teemu <teantm@utu.fi>
2024-05-17 17:38:50 +0300

    gazebo added

--- open_project/launch/launch.py
+++ open_project/launch/launch.py
@@ -1,16 +1,28 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
+from launch.actions import ExecuteProcess
+from ament_index_python.packages import get_package_share_directory
+import os
 
 def generate_launch_description():
-	return LaunchDescription([
-		Node(
-		    package='merge_map',
-		    executable='merge_map',
-		    output='screen',
-		    parameters=[{'use_sim_time': True}],
-		    remappings=[
-			("/map1", "/yourMapName1"),
-			("/map2", "/yourMapName2"),
-		    ],
-		),
-	])
+    world_path = os.path.join(get_package_share_directory('open_project'), 'worlds', 'simple.world')
+    return LaunchDescription([
+        ExecuteProcess(cmd=[
+            'gazebo',
+            '--verbose',
+            '-s', 'libgazebo_ros_init.so',  # Publish /clock
+            '-s', 'libgazebo_ros_factory.so',  # Provide gazebo_ros::Node
+            world_path
+        ], output='screen'),
+        
+        Node(
+            package='merge_map',
+            executable='merge_map',
+            output='screen',
+            parameters=[{'use_sim_time': True}],
+            remappings=[
+            ("/map1", "/yourMapName1"),
+            ("/map2", "/yourMapName2"),
+            ],
+        ),
+    ])
