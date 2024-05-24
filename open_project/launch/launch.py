0520e5dd9e1f1749b39f803dd52629d848fb6e9d
teemu <teantm@utu.fi>
2024-05-24 12:56:12 +0300

    world file

--- open_project/launch/launch.py
+++ open_project/launch/launch.py
@@ -5,7 +5,7 @@ from ament_index_python.packages import get_package_share_directory
 import os
 
 def generate_launch_description():
-    world_path = os.path.join(get_package_share_directory('open_project'), 'worlds', 'simple.world')
+    world_path = os.path.join(get_package_share_directory('open_project'), 'worlds', 'maze.world')
     return LaunchDescription([
         ExecuteProcess(cmd=[
             'gazebo',
