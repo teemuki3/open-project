615384af3a463fe56772a40c6764ba936e07be4e
teemu <teantm@utu.fi>
2024-05-29 16:25:30 +0300

    edit stuff

--- open_project/launch/open_project_launch.py
+++ open_project/launch/open_project_launch.py
@@ -20,87 +20,43 @@ import os
 
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+
+from launch_ros.actions import Node
 from launch.actions import IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import LaunchConfiguration
 
 
 def generate_launch_description():
-    world_file_dir   = os.path.join(get_package_share_directory('open_project'), 'worlds')
-    open_project_dir = os.path.join(get_package_share_directory('open_project'),'launch')
-    merge_map_dir    = os.path.join(get_package_share_directory('merge_map'),'launch')
-    pkg_slam_toolbox = get_package_share_directory('slam_toolbox')
-    pkg_gazebo_ros   = get_package_share_directory('gazebo_ros')
+
+    TURTLEBOT3_MODEL = os.environ['TURTLEBOT3_MODEL']
+    pkg_gazebo_ros = get_package_share_directory('gazebo_ros')
+    maze_world = os.path.join(get_package_share_directory('open_project'), 'worlds', 'maze.world')
+    tello_urdf = os.path.join(get_package_share_directory('open_project'), 'urdf', 'tello_seeker.urdf')
+    turtle_urdf = os.path.join(get_package_share_directory('turtlebot3_gazebo'), 'urdf', 'turtlebot3_' + TURTLEBOT3_MODEL + '.urdf')
+    turtle_sdf = os.path.join(get_package_share_directory('turtlebot3_gazebo'), 'models', 'turtlebot3_' + TURTLEBOT3_MODEL, 'model.sdf')
 
     use_sim_time = LaunchConfiguration('use_sim_time', default='true')
-    x_pose = LaunchConfiguration('x_pose', default='0.0')
-    y_pose = LaunchConfiguration('y_pose', default='0.0')
 
-    world = os.path.join(
-        world_file_dir,
-        'maze.world'
-    )
+    ld = LaunchDescription([
+        IncludeLaunchDescription(PythonLaunchDescriptionSource(os.path.join(pkg_gazebo_ros, 'launch', 'gzserver.launch.py')), launch_arguments={'world': maze_world}.items()),
+        IncludeLaunchDescription(PythonLaunchDescriptionSource(os.path.join(pkg_gazebo_ros, 'launch', 'gzclient.launch.py'))),
+        IncludeLaunchDescription(PythonLaunchDescriptionSource(os.path.join(get_package_share_directory('nav2_bringup'), 'launch', 'navigation_launch.py')), launch_arguments={'use_sim_time': use_sim_time, 'slam_params_file': get_package_share_directory('open_project')}.items()),
+        DeclareLaunchArgument('use_sim_time', default_value='true', description='Use simulation (Gazebo) clock if true'),
 
-    gzserver_cmd = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource(
-            os.path.join(pkg_gazebo_ros, 'launch', 'gzserver.launch.py')
-        ),
-        launch_arguments={'world': world}.items()
-    )
+        # Robot 1
+        Node(package='open_project', executable='drone', namespace='robot1', output='screen', parameters=[{'use_sim_time': use_sim_time}]),
+        Node(package='robot_state_publisher', executable='robot_state_publisher', namespace='robot1', output='screen', parameters=[{'use_sim_time': use_sim_time}], arguments=[tello_urdf]),
+        Node(package='gazebo_ros', executable='spawn_entity.py', arguments=['-entity', 'tello', '-file', tello_urdf, '-x', '1.0', '-y', '0.0', '-z', '1.0'], output='screen'),
 
-    gzclient_cmd = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource(
-            os.path.join(pkg_gazebo_ros, 'launch', 'gzclient.launch.py')
-        )
-    )
-    
-    slam_toolbox_cmd = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource(
-            os.path.join(pkg_slam_toolbox, 'launch', 'online_async_launch.py')
-        ),
-        launch_arguments={'use_sim_time': use_sim_time}.items()
-    )
+        # Robot 2
+        #Node(package='open_project', executable='robot', namespace='robot2', output='screen', parameters=[{'use_sim_time': use_sim_time}]), # Tässä on jotain helloworld paskaa vaan
+        Node(package='robot_state_publisher', executable='robot_state_publisher', namespace='robot2', output='screen', parameters=[{'use_sim_time': use_sim_time}], arguments=[turtle_urdf]),
+        Node(package='gazebo_ros', executable='spawn_entity.py', arguments=['-entity', TURTLEBOT3_MODEL, '-file', turtle_sdf, '-x', '0.0', '-y', '0.0', '-z', '0.0'], output='screen'),
 
-    robot_state_publisher_cmd = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource(
-            os.path.join(open_project_dir, 'robot_state_publisher.launch.py')
-        ),
-        launch_arguments={'use_sim_time': use_sim_time}.items()
-    )
+        Node(package='pointcloud_to_laserscan', executable='pointcloud_to_laserscan_node', output='screen', parameters=[os.path.join(get_package_share_directory('open_project'), 'pointcloud_to_laserscan_params.yaml')], remappings=[('cloud_in', '/depth_camera/points'), ('scan', 'ultrascan')]),
+        Node(package='slam_toolbox', executable='sync_slam_toolbox_node', output='screen', parameters=[os.path.join(get_package_share_directory('open_project'), 'slam_params.yaml')])
 
-    spawn_turtlebot_cmd = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource(
-            os.path.join(open_project_dir, 'spawn_turtlebot3.launch.py')
-        ),
-        launch_arguments={
-            'x_pose': x_pose,
-            'y_pose': y_pose
-        }.items()
-    )
-    
-    spawn_tello = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource(
-            os.path.join(open_project_dir, 'spawn_tello.py')
-        ),
-        launch_arguments={'use_sim_time': use_sim_time}.items()
-    )
-    
-    spawn_merge_map = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource(
-            os.path.join(merge_map_dir, 'merge_map_launch.py')
-        ),
-        launch_arguments={'use_sim_time': use_sim_time}.items()
-    )
-
-    ld = LaunchDescription()
-
-    # Add the commands to the launch description
-    ld.add_action(gzserver_cmd)
-    ld.add_action(gzclient_cmd)
-    ld.add_action(robot_state_publisher_cmd)
-    ld.add_action(spawn_turtlebot_cmd)
-    ld.add_action(spawn_tello)
-    ld.add_action(slam_toolbox_cmd)
-    #ld.add_action(spawn_merge_map)
-
+    ])
     return ld
